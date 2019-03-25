286853586a3f3ec838aacf003d7442289ab1ff19
Tititesouris <tititesouris@laposte.net>
2019-03-25 19:18:13 +0100

    Log a bit more than a day

--- logger.py
+++ logger.py
@@ -4,12 +4,12 @@ from sh import tail
 
 while True:
     labels = {}
-    for line in tail("-n 100", "labeldata.csv", _iter=True):
+    for line in tail("-n 2000", "labeldata.csv", _iter=True):
         labels[line.split(";")[0]] = line
 
     file = open("public/logs.csv", "w")
     try:
-        for line in tail("-n 100", "sensordata.csv", _iter=True):
+        for line in tail("-n 2000", "sensordata.csv", _iter=True):
             timestamp, temperature, humidity = line.rstrip().split(";")
             label, description = ["", ""]
             if timestamp in labels.keys():
--- public/api.php
+++ public/api.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_GET["timestamp"]) && isset($_GET["label"]) && isset($_GET["description"])) {
-    $file = "../labeldata.csv";
+    $file = __DIR__ . "/../labeldata.csv";
     $line = $_GET["timestamp"] . ";" . $_GET["label"] . ";" . $_GET["description"] . "\n";
     $fh = fopen($file, "a") or die("Can't open file");
     fwrite($fh, $line);
