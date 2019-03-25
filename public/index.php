d81a8a8cb02a7cd83d0593a31640e07d4930614b
Tititesouris <tititesouris@laposte.net>
2019-03-25 14:58:08 +0100

    Add labels to data points

--- public/index.php
+++ public/index.php
@@ -8,54 +8,102 @@
 <script src="https://www.gstatic.com/charts/loader.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-csv/0.71/jquery.csv-0.71.min.js"></script>
+<select id="marker">
+    <option value="Laundry">Put laundry to dry</option>
+    <option value="Window Open">Opened the window</option>
+    <option value="Window Closed">Closed the window</option>
+</select>
+<button id="addmarker">Add marker</button>
 <div id="temperature_chart"></div>
 <div id="humidity_chart"></div>
 <script>
-    var parseTemperature = function (value, state) {
-        if (state.colNum === 1) {
-            return new Date(value * 1000);
-        }
-        if (state.colNum === 3) {
-            return false;
-        }
-        return parseFloat(value);
-    };
-    var parseHumidity = function (value, state) {
-        if (state.colNum === 1) {
-            return new Date(value * 1000);
-        }
-        if (state.colNum === 2) {
-            return false;
-        }
-        return parseFloat(value);
+    var addMarker = function (timestamp, label, description) {
+        $.get("api.php", {timestamp: timestamp, label: label, description: description}, function (response) {
+            console.log(response);
+        });
     };
 
-    var drawTimelineChart = function (data, title, valueName, chartElement, color, format) {
+    var parseData = function (value, state) {
+        if (state.colNum === 1)
+            return new Date(value * 1000);
+        if (state.colNum > 3)
+            return value || null;
+        return value ? parseFloat(value) : null;
+    };
+    var parseTemperature = function (value, state) {
+        if (state.colNum === 3)
+            return false;
+        return parseData(value, state);
+    };
+    var parseHumidity = function (value, state) {
+        if (state.colNum === 2)
+            return false;
+        return parseData(value, state);
+    };
+
+    var drawTimelineChart = function (data, title, valueName, chartElement, color) {
+        console.log(data);
         var options = {
             title: title,
             legend: "top",
             height: 400,
-            colors: [color]
+            colors: [color],
+            hAxis: {
+                format: "HH:mm\ndd/MM/yyyy"
+            }
         };
         var dataTable = new google.visualization.DataTable();
         dataTable.addColumn("datetime", "Date");
         dataTable.addColumn("number", valueName);
+        dataTable.addColumn({type: "string", role: "annotation"});
+        dataTable.addColumn({type: "string", role: "annotationText"});
+
         dataTable.addRows(data);
+        console.log(dataTable);
 
         var chart = new google.visualization.LineChart(chartElement);
+
+        $("#addmarker").click(function () {
+            var selection = chart.getSelection()[0];
+            if (selection) {
+                addMarker(data[selection.row][0].getTime() / 1000, $("#marker").val(), $("#marker option:selected").text());
+            }
+        });
+
+        function selectHandler() {
+
+        }
+
+        google.visualization.events.addListener(chart, "select", selectHandler);
         chart.draw(dataTable, options);
     };
 
     google.charts.load("current", {
         callback: function () {
-
-
             $.get("logs.csv", function (csv) {
-                var temperatureData = $.csv.toArrays(csv, {separator: ";", onParseValue: parseTemperature});
-                drawTimelineChart(temperatureData, "Temperature over time", "Temperature °C", document.getElementById("temperature_chart"), "#c14e09");
+                var temperatureData = $.csv.toArrays(csv, {
+                    separator: ";",
+                    onParseValue: parseTemperature
+                });
+                drawTimelineChart(
+                    temperatureData,
+                    "Temperature over time",
+                    "Temperature °C",
+                    document.getElementById("temperature_chart"),
+                    "#c14e09"
+                );
 
-                var humidityData = $.csv.toArrays(csv, {separator: ";", onParseValue: parseHumidity});
-                drawTimelineChart(humidityData, "Relative humidity over time", "Relative humidity %", document.getElementById("humidity_chart"), "#006dc1");
+                var humidityData = $.csv.toArrays(csv, {
+                    separator: ";",
+                    onParseValue: parseHumidity
+                });
+                drawTimelineChart(
+                    humidityData,
+                    "Relative humidity over time",
+                    "Relative humidity %",
+                    document.getElementById("humidity_chart"),
+                    "#006dc1"
+                );
             });
         },
         packages: ["corechart"]
--- sensor.py
+++ sensor.py
@@ -88,7 +88,7 @@ am2320 = AM2320(1)
 start_time = time.time()
 while True:
     (t, h) = am2320.readSensor()
-    with open("public/logs.csv", "a") as file:
+    with open("sensordata.csv", "a") as file:
         file.write(str(time.time()) + ";" + str(t) + ";" + str(h) + "\n")
         file.close()
     time.sleep(60.0 - ((time.time() - start_time) % 60.0))
